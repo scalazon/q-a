4c3890f6150c4dc5e2a6271892cbdef6fcc7de87
Trevor Cannon <cannon.trevor@gmail.com>
2019-07-03 17:35:57 -0500

    updated outdated dependencies

--- server/server.js
+++ server/server.js
@@ -8,5 +8,5 @@ app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.listen(port, () => {
-  console.log(`server running at: http://localhost:${port}`);
+  console.log(`Serving it up at: http://localhost:${port}`);
 });
