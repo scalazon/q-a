e2b5d1b1cd1ca34e94e54790076c7c2bf79310ce
Trevor Cannon <cannon.trevor@gmail.com>
2019-07-06 12:27:21 -0500

    setting up docker containerize

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+
 const app = express();
-const port = process.env.PORT || 3001;
+const { PORT, HOST } = require('../config.js');
+
+const port = PORT || 3001;
+const host = HOST || '0.0.0.0';
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.listen(port, () => {
-  console.log(`Serving it up at: http://localhost:${port}`);
+app.listen(port, host, () => {
+  console.log(`Serving it up at: http://${host}:${port}`);
 });
