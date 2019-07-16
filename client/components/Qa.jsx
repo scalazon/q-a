dc20168a684ece789cf3157bd608ad829b5e0838
Trevor Cannon <cannon.trevor@gmail.com>
2019-07-16 16:20:27 -0500

    conditionally render ask Community button onChange of search field

--- client/components/Qa.jsx
+++ client/components/Qa.jsx
@@ -6,6 +6,7 @@ export default class Qa extends React.Component {
     super(props);
 
     this.state = {
+      askCommunity: false,
       items: [],
       alsoAsked: ['Is this a Daddle?', 'Can I use it on people AND animals?', 'WowWowowWowOwoWOwowowOW?', 'What is this?']
     };
@@ -17,43 +18,62 @@ export default class Qa extends React.Component {
 
   handleChange(e) {
     // Variable to hold the original version of the list
-let currentList = [];
+    let currentList = [];
     // Variable to hold the filtered list before putting into state
-let newList = [];
+    let newList = [];
 
     // If the search bar isn't empty
-if (e.target.value !== "") {
+    if (e.target.value !== "") {
         // Assign the original list to currentList
-  currentList = this.state.items;
+      currentList = this.state.items;
 
         // Use .filter() to determine which items should be displayed
         // based on the search terms
-  newList = currentList.filter(item => {
+      newList = currentList.filter(item => {
             // change current item to lowercase
-    const lc = item.toLowerCase();
+        const lc = item.toLowerCase();
             // change search term to lowercase
-    const filter = e.target.value.toLowerCase();
+        const filter = e.target.value.toLowerCase();
             // check to see if the current list item includes the search term
             // If it does, it will be added to newList. Using lowercase eliminates
             // issues with capitalization in search terms and search content
-    return lc.includes(filter);
-  });
-} else {
+        return lc.includes(filter);
+      });
+      } else {
         // If the search bar is empty, set newList to original task list
-  newList = this.props.items;
-}
+      newList = this.props.items;
+    }
     // Set the filtered state based on what our rules added to newList
-this.setState({
-  filtered: newList
-});
-}
+    this.setState({
+    filtered: newList
+    });
+  }
+
+
+  tester(e) {
+    e.preventDefault();
+    if (e.target.value) {
+      this.setState({ askCommunity: true})
+    }
+    console.log(e.target.value);
+  }
+
+
+
 
   render() {
     return (
         <div>
-          <h3>Have a Question?</h3>
-          <p>Find answers in product info, Q&As, reviews</p>
-          <input type="text" className="input" placeholder="Search..." />
+        <div>
+          <div>Have a Question?</div>
+          <div>Find answers in product info, Q&As, reviews</div>
+          <input type="text" className="input" placeholder="Search..." onChange={this.tester.bind(this)}/>
+        </div>
+        <div>{this.state.askCommunity ? 
+          <div><span>Don't see what you're looking for?</span><button>Ask Community?</button></div>
+          :
+          <div></div>
+          }</div>
         </div>
     );
   }
