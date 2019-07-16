bf06dbbd425cab118394e7ce3cd788846295b430
Trevor Cannon <cannon.trevor@gmail.com>
2019-07-16 14:27:09 -0500

    updated divs to reflect amazon's QA divs

--- client/components/Qa.jsx
+++ client/components/Qa.jsx
@@ -12,20 +12,10 @@ export default class Qa extends React.Component {
 
   render() {
     return (
-      <div>
         <div>
-          <h3><strong>1337DadRider</strong> "{this.state.satisfied}"</h3>
-          <p>
-            I have two dads and I'm unsure if I'll need a saddle for each, or if I can harness them both with a single saddle?
-          </p>
-          <span>Verified Buyer 2010</span>
+          <h3>Have a Question?</h3>
+          <p>Find answers in product info, Q&As, reviews</p>
         </div>
-        <div>
-          <h3><strong>Karen</strong> "{this.state.unhappy}"</h3>
-          <p>Do you even care about your customers? Wow, I bought this saddle for my 3 month old baby and he fell right out. What kind of company puts out such a negligent product with no safety testing? #mad #unblessed</p>
-          <span>Verified Buyer 2011</span>
-        </div>
-      </div>
     );
   }
 }
