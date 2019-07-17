9b041ef0ad212066a58ac366604647a7a17051c7
Trevor Cannon <cannon.trevor@gmail.com>
2019-07-17 14:44:06 -0500

    fixed issue with dotenv variables

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,10 +1,21 @@
 const mongoose = require('mongoose');
 
+const { MONGO_USER, MONGO_PASSWORD } = require('../config');
+
 mongoose.connect(
   `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@hackmazon-qu1yo.mongodb.net/hackmazon?retryWrites=true&w=majority`,
-  { useNewUrlParswer: true }
+  { useNewUrlParser: true }
 );
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {});
+db.once('open', function() {
+  console.log('connected');
+});
+
+const productSchema = new mongoose.Schema({
+  asin: String,
+  questions: [{ question: String, answer: String }]
+});
+
+const Product = mongoose.model('Product', productSchema);
